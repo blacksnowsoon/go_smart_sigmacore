a0e18f4d0346a9541f1a4f6b1bd93cbc8fdcdde7
Gharieb Khalifa <blacksnow.soon@gmail.com>
2026-03-19 23:55:42 +0000

    feat: Implement role-based approval for Module Lead and Modules Manager in SigmaCore CR, add a note field to CR requirements, and update not applicable requirements options.

--- go_smart_sigmacore/go_smart_sigmacore/doctype/sigmacore_cr/sigmacore_cr.py
+++ go_smart_sigmacore/go_smart_sigmacore/doctype/sigmacore_cr/sigmacore_cr.py
@@ -3,7 +3,18 @@
 
 # import frappe
 from frappe.model.document import Document
-
+import frappe
+from frappe import _
 
 class SigmaCoreCR(Document):
-	pass
+	def validate(self):
+		if self.module_lead_approval and not self.has_role("SigmaCore Module Lead"):
+			if not self._doc_before_save or not self._doc_before_save.module_lead_approval:
+				frappe.throw(_("Only the Module Lead can check Lead Approved"))
+		
+		if self.modules_manager_approval and not self.has_role("SigmaCore Modules Manager"):
+			if not self._doc_before_save or not self._doc_before_save.modules_manager_approval:
+				frappe.throw(_("Only the Modules Manager can check Modules Manager Approved"))
+
+	def has_role(self, role):
+		return role in frappe.get_roles(frappe.session.user)
