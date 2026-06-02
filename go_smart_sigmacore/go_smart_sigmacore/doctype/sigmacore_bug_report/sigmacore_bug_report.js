bb91297a540ac9314fcb37219fa96f24226e7eba
Gharieb Khalifa <blacksnow.soon@gmail.com>
2026-06-02 10:15:04 +0300

    feat: implement dynamic filtering and field access control for child table modules in bug report and CR doctypes

--- go_smart_sigmacore/go_smart_sigmacore/doctype/sigmacore_bug_report/sigmacore_bug_report.js
+++ go_smart_sigmacore/go_smart_sigmacore/doctype/sigmacore_bug_report/sigmacore_bug_report.js
@@ -2,6 +2,43 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on("SigmaCore Bug Report", {
+	onload(frm) {
+        //filter level 1 
+        frm.set_query('module_name', 'impact_area', function(doc, cdt, cdn) {
+            let row = locals[cdt][cdn];
+            return {
+                'filters': {
+                    'is_group': '1',
+                    'hierarchy_level': 'Module'
+                }
+            };
+        });
+        //filter level 2
+        frm.set_query('section', 'impact_area', function(doc, cdt, cdn) {
+            let row = locals[cdt][cdn];
+            
+            return {
+                'filters' : {
+                    'is_group': '1',
+                    'parent_sigmacore_module': row.module_name,
+                    'hierarchy_level': 'Section'
+                }
+            };
+        });
+        //filter level 3
+        frm.set_query('target', 'impact_area', function(doc, cdt, cdn) {
+            let row = locals[cdt][cdn];
+            
+            return {
+                'filters' : {
+                    'is_group': '0',
+                    'parent_sigmacore_module': row.section,
+                    // 'hierarchy_level': 'Module'
+                }
+            };
+        });
+        
+    },
 	refresh(frm) {
 		frm.fields_dict["report_preview"].tab.tab_link.on("click", () => {
 			render_bug_report_preview(frm);
@@ -11,6 +48,76 @@ frappe.ui.form.on("SigmaCore Bug Report", {
 		})
 	},
 });
+
+
+
+frappe.ui.form.on("SigmaCore Module Scope Child Table", {
+    impact_area_add(frm, cdt, ndt) {
+        frm.fields_dict.impact_area.grid.update_docfield_property(
+            "section", "read_only", 1
+        );
+         frm.fields_dict.impact_area.grid.update_docfield_property(
+            "target", "read_only", 1
+        );
+        
+      frm.refresh_field('impact_area');
+    },
+	module_name: function(frm,cdt, ndt) {
+	    const row = locals[cdt][ndt];
+	    
+	    if (!row.module_name && (row.section || row.target)){
+	        row.section = "";
+	        row.target = "";
+	    }
+	    
+	    frm.fields_dict.impact_area.grid.update_docfield_property(
+            "section", "read_only", !row.module_name ? 1 : 0
+        );
+	    frm.fields_dict.impact_area.grid.update_docfield_property(
+            "target", "read_only", !row.module_name ? 1 : 0
+        );
+	    frm.refresh_field('impact_area');
+	},
+	section: function(frm,cdt, ndt) {
+	    const row = locals[cdt][ndt];
+	   if (!row.section) row.target = "";
+	   
+	   frm.fields_dict.imapct_area.grid.update_docfield_property(
+            "target", "read_only", !row.section ? 1 : 0
+        );
+	    frm.refresh_field('impact_area');
+	}
+});
+
+function childTableAccessCondations(frm) {
+    // map each row
+        frm.fields_dict.impact_area.grid.grid_rows.map(row => {
+            // map the columns of the row
+            for (let [key, value] of Object.entries(row.columns)) {
+                // map the meta data of each column or cell 
+                for (let [k, v] of Object.entries(value)) {
+                    // the field property is added as soon as you toch the row as an object 
+                    // if it has that property search the value prop to check if empty or not.
+                    // from the df prop you can change the field properties(read_only, reqd, ...etc)
+                    // here i want to disable the section field if the module_name is empty
+                    if(k === "field" && key === "module_name") {
+                        if(v === undefined || v === "") {
+                            row.columns.section.df.read_only = 1;
+                        } else {
+                            row.columns.section.df.read_only = 0;
+                        }
+                        
+                    } else {
+                         row.columns.section.df.read_only = 1;
+                    }
+                    
+                }
+                
+            }
+            
+        })
+}
+
 
 function render_bug_report_preview(frm) {
 	if (frm.doc.__islocal) {
--- go_smart_sigmacore/go_smart_sigmacore/doctype/sigmacore_cr/sigmacore_cr.js
+++ go_smart_sigmacore/go_smart_sigmacore/doctype/sigmacore_cr/sigmacore_cr.js
@@ -2,6 +2,43 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on("SigmaCore CR", {
+	onload(frm) {
+        //filter level 1 
+        frm.set_query('module_name', 'scopes', function(doc, cdt, cdn) {
+            let row = locals[cdt][cdn];
+            return {
+                'filters': {
+                    'is_group': '1',
+                    'hierarchy_level': 'Module'
+                }
+            };
+        });
+        //filter level 2
+        frm.set_query('section', 'scopes', function(doc, cdt, cdn) {
+            let row = locals[cdt][cdn];
+            
+            return {
+                'filters' : {
+                    'is_group': '1',
+                    'parent_sigmacore_module': row.module_name,
+                    'hierarchy_level': 'Section'
+                }
+            };
+        });
+        //filter level 3
+        frm.set_query('target', 'scopes', function(doc, cdt, cdn) {
+            let row = locals[cdt][cdn];
+            
+            return {
+                'filters' : {
+                    'is_group': '0',
+                    'parent_sigmacore_module': row.section,
+                    // 'hierarchy_level': 'Module'
+                }
+            };
+        });
+        
+    },
 	refresh(frm) {
 		frm.fields_dict["cr_preview"].tab.tab_link.on("click", () => {
 			render_cr_preview(frm);
@@ -10,8 +47,88 @@ frappe.ui.form.on("SigmaCore CR", {
 			
 			render_pdf_view(frm)
 		})
+		if (frm.is_new()) {
+            
+            frm.fields_dict.scopes.grid.update_docfield_property(
+                "section", "read_only", 1
+            );
+             frm.fields_dict.scopes.grid.update_docfield_property(
+                "target", "read_only", 1
+            );
+        }
 	},
 });
+
+
+
+frappe.ui.form.on("SigmaCore Module Scope Child Table", {
+    scopes_add(frm, cdt, ndt) {
+        frm.fields_dict.scopes.grid.update_docfield_property(
+            "section", "read_only", 1
+        );
+         frm.fields_dict.scopes.grid.update_docfield_property(
+            "target", "read_only", 1
+        );
+        
+      frm.refresh_field('scopes');
+    },
+	module_name: function(frm,cdt, ndt) {
+	    const row = locals[cdt][ndt];
+	    
+	    if (!row.module_name && (row.section || row.target)){
+	        row.section = "";
+	        row.target = "";
+	    }
+	    
+	    frm.fields_dict.scopes.grid.update_docfield_property(
+            "section", "read_only", !row.module_name ? 1 : 0
+        );
+	    frm.fields_dict.scopes.grid.update_docfield_property(
+            "target", "read_only", !row.module_name ? 1 : 0
+        );
+	    frm.refresh_field('scopes');
+	},
+	section: function(frm,cdt, ndt) {
+	    const row = locals[cdt][ndt];
+	   if (!row.section) row.target = "";
+	   
+	   frm.fields_dict.scopes.grid.update_docfield_property(
+            "target", "read_only", !row.section ? 1 : 0
+        );
+	    frm.refresh_field('scopes');
+	}
+});
+
+function childTableAccessCondations(frm) {
+    // map each row
+        frm.fields_dict.scopes.grid.grid_rows.map(row => {
+            // map the columns of the row
+            for (let [key, value] of Object.entries(row.columns)) {
+                // map the meta data of each column or cell 
+                for (let [k, v] of Object.entries(value)) {
+                    // the field property is added as soon as you toch the row as an object 
+                    // if it has that property search the value prop to check if empty or not.
+                    // from the df prop you can change the field properties(read_only, reqd, ...etc)
+                    // here i want to disable the section field if the module_name is empty
+                    if(k === "field" && key === "module_name") {
+                        if(v === undefined || v === "") {
+                            row.columns.section.df.read_only = 1;
+                        } else {
+                            row.columns.section.df.read_only = 0;
+                        }
+                        
+                    } else {
+                         row.columns.section.df.read_only = 1;
+                    }
+                    
+                }
+                
+            }
+            
+        })
+}
+
+
 
 function render_cr_preview(frm) {
 	if (frm.doc.__islocal) {
